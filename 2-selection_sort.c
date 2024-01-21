5f3084b296e05bbde59b15cd0b4bd5869e6c0a91
Kananmrzzd <kananmirzazada@gmail.com>
2024-01-21 13:38:24 +0400

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers with
+ *                  Selection sort algorithm
+ * @array: given integers
+ * @size: size of array
+ *
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+        size_t i, j, tmpI;
+        int tmp;
+
+        if (!array || size < 2)
+                return;
+
+        for (i = 0; i < size; i++)
+        {
+                tmp = array[i];
+                tmpI = i;
+
+                for (j = i; j < size; j++)
+                {
+                        if (tmp > array[j])
+                        {
+                                tmp = array[j];
+                                tmpI = j;
+                        }
+                }
+                array[tmpI] = array[i];
+                array[i] = tmp;
+
+                print_array(array, size);
+        }
+}
