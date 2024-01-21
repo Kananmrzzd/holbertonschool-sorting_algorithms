733910859414d417ba3935169b4a3e90f35edeb0
root <gqasimova93@gmail.com>
2024-01-21 06:20:17 -0800

    using the Insertion sort algorithm

--- sort.h
+++ sort.h
@@ -18,10 +18,6 @@ void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-<<<<<<< HEAD
-=======
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
->>>>>>> 9710ff8051e45b9779e779434f7735eed82d251b
 #endif
